984ae28a87c334cbb280ab309a5d6ea0957a63f8
sctnightcore <nightcoremax@gmail.com>
2019-03-16 16:36:08 +0700

    add check $b

--- Lib/AntiCaptcha.pm
+++ Lib/AntiCaptcha.pm
@@ -7,6 +7,13 @@ sub anti_captcha {
 	my $recognizer = WebService::Antigate::V2->new(key => $antikey , api_version => 2);
 	my $ans = $recognizer->upload_and_recognize(file => "img/$checksum.png") or die $recognizer->errstr;
 	my $b = $recognizer->balance();
+
+	if ($b == '0') {
+		print "You balance in Anti-Captcha.com is $b ! \n";
+		exit;
+	}
+	
 	return ($ans,$b);
 }
+
 1;
