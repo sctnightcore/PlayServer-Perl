8ad69ceca730c6b43a28eec5f3fb38084fb36141
sctnightcore <nightcoremax@gmail.com>
2019-03-19 22:50:38 +0700

    Update AntiCaptcha.pm

--- Lib/AntiCaptcha.pm
+++ Lib/AntiCaptcha.pm
@@ -1,17 +1,15 @@
 package AntiCaptcha;
-
 use WebService::Antigate::V2;
+my $recognizer = WebService::Antigate::V2->new(key => $antikey , api_version => 2);
 
 sub anti_captcha {
 	my ($checksum,$antikey) = @_;
-	my $recognizer = WebService::Antigate::V2->new(key => $antikey , api_version => 2);
 	my $ans = $recognizer->upload_and_recognize(file => "img/$checksum.png") or die $recognizer->errstr;
 	return $ans;
 }
 
 sub checkmoney {
 	my ($antikey) = @_;
-	my $recognizer = WebService::Antigate::V2->new(key => $antikey , api_version => 2);
 	my $b = $recognizer->balance();
 	return $b;
 }
