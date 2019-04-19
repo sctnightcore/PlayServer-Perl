bc4833190d6b0625309e6da5b8bce2e2c957f1bc
sctnightcore <nightcoremax@gmail.com>
2019-04-19 23:23:17 +0700

    Add Debug with socket !

--- PlayServer-Perl.pl
+++ PlayServer-Perl.pl
@@ -12,7 +12,7 @@ use lib "$RealBin/Lib";
 use AntiCaptcha;
 use File;
 use PlayServer;
-
+use SocketClient;
 my $cfg = Config::IniFiles->new( -file => "config.ini" );
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
 my $now_string = strftime "%H:%M:%S", localtime;
@@ -31,22 +31,29 @@ sub Start {
 	File::clear_oldchecksum();
 	check_config();
 	my $hash_data;
+	my $debug = SocketClient->new();
 	my $playserver = PlayServer->new( GameID => $cfg->val( 'Setting', 'GAMEID' ), ServerID => $cfg->val('Setting','SERVERID'));
 	my $anticaptcha = AntiCaptcha->new( anticaptcha_key => $cfg->val('Setting','AntiCaptchakey'));
 	my ($startsendagain,$success,$fail,$waitsend,$count) = (0,0,0,0,0);
 	$playserver->getserver_link();	
-	while () {
+	while (1) {
 		update_titlebar('[ Count: '.$count.' | Success: '.$success.' | Fail: '.$fail.' | WaitSend: '.$waitsend.' ] BY SCTNIGHTCORE');
-		my $balance = $anticaptcha->checkbalance();
+		#my $balance = $anticaptcha->checkbalance();
 		#Get checksun
 		my $checksum = $playserver->getimg_saveimg();
+		#debug 
+		$debug->sendSocket("[Get_Checksum!]:$checksum");
 		#check if have checksum file 
 		if (defined $checksum) {
 			#get taskID / get answer
 			my ($taskid,$answer) = $anticaptcha->get_taskid_and_answer($checksum);
 			#my $answer = inputfromkeyboard(); #for test ! 
+			#debug
+			$debug->sendSocket("[Get_Answer_TaskId!]: $answer | $taskid");	
 			# remove checksum file
-			File::file_remove($checksum);		
+			File::file_remove($checksum);
+			#debug 
+			$debug->sendSocket("[ADD DATA!]CHECKSUM:$checksum|TASKID:$taskid|ANSWER:$answer");
 			#push checksum / answer to hashdata
 			push (@{$hash_data->{all_data}},{ checksum => $checksum, answer => $answer, taskid => $taskid});
 			#update var
@@ -60,14 +67,20 @@ sub Start {
 				$waitsend -= 1;
 				#send answer
 				my $res_playserver = $playserver->send_answer($hash_data->{all_data}->[0]->{answer}, $hash_data->{all_data}->[0]->{checksum});
+				#debug 
+				$debug->sendSocket("[send_Checksum!]:$hash_data->{all_data}->[0]->{checksum} | $hash_data->{all_data}->[0]->{answer}");				
 				#check res playserver
 				#0 = Fail / 1 = Success
 				if ($res_playserver->{'success'}) {
 					print "[\e[1;37m$now_string\e[0m] - [\e[1;42;1mSUCCESS\e[0m] | [\e[1;37mCHECKSUM:\e[0m $hash_data->{all_data}->[0]->{checksum}] | [\e[1;37mANSWER:\e[0m $hash_data->{all_data}->[0]->{answer}]\n";
-					$success += 1;	
+					$success += 1;
+					#debug
+					$debug->sendSocket("[send_Checksum_Success!]: $hash_data->{all_data}->[0]->{checksum} | $hash_data->{all_data}->[0]->{answer}");					
 				} else {
 					print "[\e[1;37m$now_string\e[0m] - [\e[1;41;1mFail\e[0m] | [\e[1;37mCHECKSUM:\e[0m $hash_data->{all_data}->[0]->{checksum}] | [\e[1;37mANSWER:\e[0m $hash_data->{all_data}->[0]->{answer}]\n";
 					$fail += 1;
+					#debug
+					$debug->sendSocket("[send_Checksum_Fail!]: $hash_data->{all_data}->[0]->{checksum} | $hash_data->{all_data}->[0]->{answer}");
 					#TODO config auto report 
 					$anticaptcha->report_imgcaptcha($hash_data->{all_data}->[0]->{taskid});
 
@@ -119,6 +132,7 @@ sub Load_lib {
 	require AntiCaptcha;
 	require File;
 	require PlayServer;
+	require SocketClient;
 	require WWW::Mechanize;
 	require WebService::Antigate;
 	require Term::ANSIColor;
