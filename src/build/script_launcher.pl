a88e5642c8b29b6ca0340eeeed13df3cc5f40096
sctnightcore <nightcoremax@gmail.com>
2019-06-01 04:25:52 +0700

    rebuild with win10

--- src/build/script_launcher.pl
+++ src/build/script_launcher.pl
@@ -49,7 +49,6 @@ if (0) {
 	require HTTP::Request::Common;
 	require MIME::Base64;
 	require WebService::AntiCaptcha;
-	require Data::Dumper;
 	require Win32::Console::ANSI;
 	require Win32::Console;
 	require Dir::Self;
